53ce2694912c45bb79f64e755540db77742e17cb
ElieneRodriguesLima <63461960+ElieneRodriguesLima@users.noreply.github.com>
2020-08-19 20:22:35 -0300

    Criei o arquivo Nota.py

---
+++ Nota.py
@@ -0,0 +1,9 @@
+# Questão 01
+# È solicitado ao usuário um nome
+nome = str(input('Digite um nome: '))
+
+# Retorna o nome e o comprimento
+caract = len(nome)
+
+#Imprimi ao usuário o comprimento de itens no nome informado
+print(f' O nome {nome} possui {caract} caracteres. ')
